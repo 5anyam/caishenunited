2fc02182f89c15a58e6ac4aaf04fb529a383ee76
Sanyam <you@example.comsanyamgulia@gmail.com>
2026-01-01 20:06:46 +0530

    Add new files

--- components/anouncement.tsx
+++ components/anouncement.tsx
@@ -20,11 +20,11 @@ export default function AnnouncementBar() {
     <span className="inline-flex items-center gap-2 whitespace-nowrap px-4 py-2">
       <Tag className="w-4 h-4 text-gray-300 flex-shrink-0" />
       <span className="text-xs sm:text-sm text-white font-light tracking-wide leading-tight">
-        New Customer Offer: Get{' '}
+      Use discount code - Get{' '}
         <span className="font-semibold text-white px-2.5 py-1 bg-white/20 rounded-md shadow-sm">
           10% OFF
         </span>
-        {' '}your first order with code{' '}
+        {' '} and get Flat 10% off with more Exciting Offers.{' '}
         <span className="font-semibold tracking-wider text-white/90">NEWBEGIN10</span>
       </span>
     </span>
